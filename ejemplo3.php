617192b3585fe1768cb1ae2401ba5ddd996b818d
usuario <jesusdavid.guti@gmail.com>
2021-01-18 19:29:19 +0100

    usuario

--- ejemplo3.php
+++ ejemplo3.php
@@ -1,11 +1,12 @@
 <?php
 //ejemplo3.php
 $bd = new PDO('mysql:host=localhost;dbname=dwes;charset=utf8', 'dwes', 'abc123');
-
+// Mail
 if (isset($_GET['q'])) {
     $consulta = $bd->prepare("SELECT id FROM users WHERE lower(email) = lower(:email)");
     $consulta->execute(['email' => $_GET['q']]);
-    if ($provincia = $consulta->fetch()) {
+    $idBD = $consulta->fetch();    
+    if ($idBD != null) {
         echo "El correo ya existe";
     } else {
         echo "OK";
@@ -14,4 +15,18 @@ if (isset($_GET['q'])) {
     else {
         echo "";
     }
+// Usuario
+if (isset($_GET['u'])) {
+        $consulta = $bd->prepare("SELECT id FROM users WHERE lower(username) = lower(:username)");
+        $consulta->execute(['username' => $_GET['u']]);
+        $idBD = $consulta->fetch();    
+        if ($idBD != null) {
+            echo "El usuario ya existe";
+        } else {
+            echo "OK";
+            }
+        } 
+        else {
+            echo "";
+        }
 ?>
