197a4cae27f4da5482391eff8a1d1524e9d7f103
usuario <jesusdavid.guti@gmail.com>
2021-03-09 07:41:37 +0100

    Examen

--- examen/funciones.js
+++ examen/funciones.js
@@ -1,4 +1,42 @@
-function cargar_productos() {
+function buscaProducto(str) {
+    
+    let puntero = document.getElementById("areaMensaje");
+    let cadena = "";
+
+    if (str.length == 0) {
+        document.getElementById("areaMensaje").innerHTML = "";
+        return;
+    } else {
+
+        fetch('busquedaServidor.php?q=' + str)
+            .then(function(response) { 
+                return response.json(); // Este devuelve un json
+                //return response.text(); // Este response devuelve los datos en crudo.
+            })
+            .then(function(producto) { // lo recibimos en el parámetro "producto"
+
+                if (producto.length > 0){
+                    cadena = "Producto existe con id = " + producto[0].id
+                }
+                else{
+                    cadena = "";
+                }
+
+                puntero.innerHTML = cadena;
+
+                /* document.getElementById("spnNombreCorto").innerHTML=
+                producto.nombre_corto;
+                document.getElementById("spnDescripcion").innerHTML=
+                producto.descripcion;
+                document.getElementById("spnPVP").innerHTML=
+                producto.PVP;
+                document.getElementById("ficha").style.display = "block";  */               
+            })
+            .catch();
+    }
+}
+
+function cargarProductos() {
     
     let puntero = document.getElementById("areaDatos");
     let cadena = "";
@@ -10,14 +48,14 @@ function cargar_productos() {
             return response.json(); // Este devuelve un json
             //return response.text(); // Este response devuelve los datos en crudo.
         })
-        .then(function(productos) { // lo recibimos en el parámetro "noticia"
+        .then(function(productos) { // lo recibimos en el parámetro "productos"
 
             // Mostrar datos HTML
 
             cadena = "<ul>";
 
-            array.forEach(productos => {
-                cadena += "<li>" + productos.producto + "</li>"
+            productos.forEach(producto => {
+                cadena += "<li>" + producto.producto + "</li>"
             });
 
             cadena += "</ul>";
